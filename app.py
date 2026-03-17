0155d895c958c5c5476de188987a82c00823dad7
tuhsar__3353 <tusharsathe6626@gmail.com>
2026-03-17 13:21:06 +0530

    updated project fileupdated project file

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, request, redirect
-import psycopg2
+# from flask import Flask, render_template, request, redirect, session
+# import psycopg2
 
-app = Flask(__name__)
+# app = Flask(__name__)
+# app.secret_key = "secret123"
 
-# DATABASE CONNECTION
-conn = psycopg2.connect(
-    host="localhost",
-    database="online_bike_portal",
-    user="postgres",
-    password="root"
-)
+# # DATABASE CONNECTION
 
-# ================= HOME + SEARCH =================
+# conn = psycopg2.connect(
+# host="localhost",
+# database="online_bike_portal",
+# user="postgres",
+# password="root"
+# )
+
+# # ---------------- HOME PAGE ----------------
+
 # @app.route("/")
 # def home():
 
@@ -19,10 +22,155 @@ conn = psycopg2.connect(
 
 #     cur = conn.cursor()
 
-#     # SEARCH LOGIC
 #     if search:
 #         cur.execute(
-#             "SELECT * FROM bikes WHERE LOWER(name) LIKE LOWER(%s)",
+#         "SELECT * FROM bikes WHERE bike_name ILIKE %s",
+#         ('%' + search + '%',)
+#     )
+#     else:
+#         cur.execute("SELECT * FROM bikes")
+
+#     bikes = cur.fetchall()
+#     cur.close()
+
+#     return render_template("index.html", bikes=bikes)
+
+# # ---------------- OFFER PAGE ----------------
+
+# @app.route("/offer/[int:bike_id](int:bike_id)")
+# def offer_page(bike_id):
+
+#     cur = conn.cursor()
+
+#     cur.execute(
+#     "SELECT * FROM bikes WHERE id=%s",
+#     (bike_id,)
+# )
+
+#     bike = cur.fetchone()
+
+#     cur.close()
+
+#     return render_template("offer.html", bike=bike)
+
+# # ---------------- BRAND SEARCH ----------------
+
+# @app.route("/brand")
+# def brand():
+
+#     brand = request.args.get("brand")
+
+#     cur = conn.cursor()
+
+#     cur.execute(
+#     "SELECT * FROM bikes WHERE brand ILIKE %s",
+#     ('%' + brand + '%',)
+#     )
+
+#     bikes = cur.fetchall()
+
+#     cur.close()
+
+#     return render_template("index.html", bikes=bikes)
+
+# # ---------------- BUDGET SEARCH ----------------
+
+# @app.route("/budget")
+# def budget():
+
+#     price = request.args.get("price")
+
+#     cur = conn.cursor()
+
+#     cur.execute(
+#     "SELECT * FROM bikes WHERE price <= %s",
+#     (price,)
+#     )
+
+#     bikes = cur.fetchall()
+
+#     cur.close()
+
+#     return render_template("index.html", bikes=bikes)
+
+# # ---------------- LOGIN ----------------
+
+# @app.route("/login", methods=["POST"])
+# def login():
+
+#     username = request.form["username"]
+#     password = request.form["password"]
+
+#     cur = conn.cursor()
+
+#     cur.execute(
+#     "SELECT * FROM users WHERE username=%s AND password=%s",
+#     (username, password)
+#     )
+
+#     user = cur.fetchone()
+
+#     cur.close()
+
+#     if user:
+#         session["user"] = username
+
+#     return redirect("/")
+
+# # ---------------- REGISTER ----------------
+
+# @app.route("/register", methods=["POST"])
+# def register():
+
+#     username = request.form["username"]
+#     email = request.form["email"]
+#     password = request.form["password"]
+
+#     cur = conn.cursor()
+
+#     cur.execute(
+#         "INSERT INTO users(username,email,password) VALUES(%s,%s,%s)",
+#         (username, email, password)
+#     )
+
+#     conn.commit()
+
+#     cur.close()
+
+#     return redirect("/")
+
+# # ---------------- RUN SERVER ----------------
+
+# if __name__ == "**main**":
+#     app.run(debug=True)
+
+
+
+
+# from flask import Flask, render_template, request, redirect
+# import psycopg2
+
+# app = Flask(__name__)
+
+# # DATABASE CONNECTION
+# conn = psycopg2.connect(
+#     host="localhost",
+#     database="online_bike_portal",
+#     user="postgres",
+#     password="root"
+# )
+
+# # ---------------- HOME PAGE ----------------
+# @app.route("/")
+# def home():
+
+#     search = request.args.get("search")
+
+#     cur = conn.cursor()
+
+#     if search:
+#         cur.execute(
+#             "SELECT * FROM bikes WHERE bike_name ILIKE %s",
 #             ('%' + search + '%',)
 #         )
 #     else:
@@ -34,6 +182,60 @@ conn = psycopg2.connect(
 #     return render_template("index.html", bikes=bikes)
 
 
+# # ---------------- OFFER PAGE ----------------
+# @app.route("/offer/<int:bike_id>")
+# def offer_page(bike_id):
+
+#     cur = conn.cursor()
+#     cur.execute(
+#         "SELECT * FROM bikes WHERE bike_id=%s",
+#         (bike_id,)
+#     )
+
+#     bike = cur.fetchone()
+#     cur.close()
+
+#     return render_template("offer.html", bike=bike)
+
+
+# # ---------------- CATEGORY FILTER ----------------
+# @app.route("/category/<cc>")
+# def category(cc):
+
+#     cur = conn.cursor()
+#     cur.execute(
+#         "SELECT * FROM bikes WHERE category=%s",
+#         (cc,)
+#     )
+
+#     bikes = cur.fetchall()
+#     cur.close()
+
+#     return render_template("index.html", bikes=bikes)
+
+
+# # ---------------- RUN SERVER ----------------
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+
+
+from flask import Flask, render_template, request, redirect, session
+import psycopg2
+
+app = Flask(__name__)
+app.secret_key = "secret123"
+
+# DATABASE CONNECTION
+conn = psycopg2.connect(
+    host="localhost",
+    database="online_bike_portal",
+    user="postgres",
+    password="root"
+)
+
+# ---------------- HOME PAGE ----------------
 @app.route("/")
 def home():
 
@@ -41,32 +243,95 @@ def home():
 
     cur = conn.cursor()
 
-    try:
+    if search:
+        cur.execute(
+            "SELECT * FROM bikes WHERE bike_name ILIKE %s",
+            ('%' + search + '%',)
+        )
+    else:
+        cur.execute("SELECT * FROM bikes")
 
-        if search:
-            cur.execute(
-                "SELECT * FROM bikes WHERE bike_name ILIKE %s",
-                ('%' + search + '%',)
-            )
-        else:
-            cur.execute("SELECT * FROM bikes")
-
-        bikes = cur.fetchall()
-        cur.close()
-
-        return render_template("index.html", bikes=bikes)
-
-    except Exception as e:
-        conn.rollback()
-        print("Database Error:", e)
-        bikes = []
-
+    bikes = cur.fetchall()
     cur.close()
 
     return render_template("index.html", bikes=bikes)
 
 
-# ================= REGISTER =================
+# ---------------- OFFER PAGE ----------------
+@app.route("/offer/<int:bike_id>")
+def offer_page(bike_id):
+
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM bikes WHERE bike_id=%s",
+        (bike_id,)
+    )
+
+    bike = cur.fetchone()
+    cur.close()
+
+    return render_template("offer.html", bike=bike)
+
+
+# ---------------- BRAND SEARCH ----------------
+@app.route("/brand")
+def brand():
+
+    brand = request.args.get("brand")
+
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM bikes WHERE brand ILIKE %s",
+        ('%' + brand + '%',)
+    )
+
+    bikes = cur.fetchall()
+    cur.close()
+
+    return render_template("index.html", bikes=bikes)
+
+
+# ---------------- BUDGET SEARCH ----------------
+@app.route("/budget")
+def budget():
+
+    price = request.args.get("price")
+
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM bikes WHERE price <= %s",
+        (price,)
+    )
+
+    bikes = cur.fetchall()
+    cur.close()
+
+    return render_template("index.html", bikes=bikes)
+
+
+# ---------------- LOGIN ----------------
+@app.route("/login", methods=["POST"])
+def login():
+
+    username = request.form["username"]
+    password = request.form["password"]
+
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM users WHERE username=%s AND password=%s",
+        (username, password)
+    )
+
+    user = cur.fetchone()
+    cur.close()
+
+    if user:
+        session["user"] = username
+
+    return redirect("/")
+
+
+# ---------------- REGISTER ----------------
 @app.route("/register", methods=["POST"])
 def register():
 
@@ -85,70 +350,6 @@ def register():
     return redirect("/")
 
 
-# ================= LOGIN =================
-@app.route("/login", methods=["POST"])
-def login():
-
-    username = request.form["username"]
-    password = request.form["password"]
-
-    cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM users WHERE username=%s AND password=%s",
-        (username, password)
-    )
-
-    user = cur.fetchone()
-    cur.close()
-
-    if user:
-        print("Login Successful")
-    else:
-        print("Invalid Login")
-
-    return redirect("/")
-
-
-# ================= OFFER PAGE =================
-@app.route("/offer/<int:bike_id>")
-def offer_page(bike_id):
-
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM bikes WHERE id=%s", (bike_id,))
-    bike = cur.fetchone()
-    cur.close()
-
-    return render_template("offer.html", bike=bike)
-
-
-# ================= SUBMIT OFFER =================
-@app.route("/submit_offer", methods=["POST"])
-def submit_offer():
-
-    name = request.form["name"]
-    phone = request.form["phone"]
-    bike_id = request.form["bike_id"]
-
-    cur = conn.cursor()
-    cur.execute(
-        "INSERT INTO offers(name,phone,bike_id) VALUES(%s,%s,%s)",
-        (name, phone, bike_id)
-    )
-    conn.commit()
-    cur.close()
-
-    return redirect("/")
-
-
+# ---------------- RUN SERVER ----------------
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-try:
-    cur.execute("SELECT * FROM bikes")
-    bikes = cur.fetchall()
-
-except Exception as e:
-    conn.rollback()
-    print(e)
